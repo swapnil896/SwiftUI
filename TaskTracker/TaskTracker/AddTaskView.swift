190f4f58a1d05aa2b01156fac330d172aeb7f9d8
Swapnil Magar <magarswapnil896@gmail.com>
2026-04-22 00:09:41 +0530

    Added Edit and Delete buttons

--- TaskTracker/TaskTracker/AddTaskView.swift
+++ TaskTracker/TaskTracker/AddTaskView.swift
@@ -7,9 +7,17 @@
 
 import SwiftUI
 
+enum TaskMode {
+    case add, edit
+}
+
 struct AddTaskView: View {
     @State private var title: String = ""
     @State private var description: String = ""
+    @Binding var taskList: [TaskModel]
+    @Binding var mode: TaskMode
+    @Binding var selectedTask: TaskModel?
+    @Environment(\.dismiss) var dismiss
     
     var body: some View {
         VStack() {
@@ -17,7 +25,7 @@ struct AddTaskView: View {
                 Text("Title")
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .font(.headline)
-                TextField("Enter task title", text: $title)
+                TextField("Enter task title", text: getTitleText())
                     .padding()
                     .overlay(
                         RoundedRectangle(cornerRadius: 8)
@@ -29,7 +37,7 @@ struct AddTaskView: View {
                 Text("Description")
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .font(.headline)
-                PlaceholderTextEditorView(text: $description, placeholder: "Enter your Description here")
+                PlaceholderTextEditorView(text: getDescriptionText(), placeholder: "Enter your Description here")
                     .frame(height: 300)
                     .overlay(
                         RoundedRectangle(cornerRadius: 8)
@@ -38,7 +46,9 @@ struct AddTaskView: View {
             }
             .padding()
             Spacer(minLength: 10)
-            Button("Save") {
+            Button("Save" + (mode == .edit ? " changes" : "")) {
+                saveTask()
+                dismiss()
                 
             }
             .padding()
@@ -49,9 +59,51 @@ struct AddTaskView: View {
             .navigationTitle("Add your Task")
         }
     }
-}
-
-#Preview {
-    AddTaskView()
+    
+    func getTitleText() -> Binding<String> {
+        switch mode {
+        case .add:
+            return $title
+        case .edit:
+            return Binding(
+                get: { selectedTask?.title ?? "" },
+                set: { selectedTask?.title = $0 }
+            )
+        }
+    }
+    
+    func getDescriptionText() -> Binding<String> {
+        switch mode {
+        case .add:
+            return $description
+        case .edit:
+            return Binding {
+                selectedTask?.description ?? ""
+            } set: { newVal in
+                selectedTask?.description = newVal
+            }
+        }
+    }
+    
+    func saveTask() {
+        if title.isEmpty {
+            // show alert
+            return
+        }
+        switch mode {
+        case .add:
+            let newTask = TaskModel(title: title, description: description)
+            taskList.append(newTask)
+        case .edit:
+            if var task = selectedTask {
+                task.title = title
+                task.description = description
+                if let index = taskList.firstIndex(where: { $0.id == task.id }) {
+                    taskList[index] = task
+                    selectedTask = task
+                }
+            }
+        }
+    }
 }
 
--- TaskTracker/TaskTracker/ContentView.swift
+++ TaskTracker/TaskTracker/ContentView.swift
@@ -7,27 +7,100 @@
 
 import SwiftUI
 
+struct TaskModel: Identifiable {
+    let id = UUID()
+    var title: String
+    var description: String
+    var isSelectedForEdit: Bool = false
+}
+
 struct ContentView: View {
     @State private var navigateToAddTask: Bool = false
+    @State private var taskList: [TaskModel] = []
+    @State private var mode: TaskMode = .add
+    @State private var selectedTask: TaskModel? = nil
     
     var body: some View {
         NavigationStack {
-            VStack {
-                Button("Add Task") {
-                    navigateToAddTask = true
+            Group {
+                if taskList.isEmpty {
+                    emptyStateView
+                } else {
+                    taskListView
                 }
-                .frame(width: 200)
-                .padding()
-                .background(Color.blue)
-                .cornerRadius(6)
-                .foregroundColor(.white)
-                
             }
-            .padding()
             .navigationDestination(isPresented: $navigateToAddTask) {
-                AddTaskView()
+                AddTaskView(taskList: $taskList, mode: $mode, selectedTask: $selectedTask)
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    if !taskList.isEmpty {
+                        Button("Add Task") {
+                            navigateToAddTask = true
+                        }
+                    }
+                }
             }
         }
+    }
+    
+    @ViewBuilder
+    var emptyStateView: some View {
+        VStack {
+            Text("No tasks yet!")
+                .font(.headline)
+                .foregroundColor(.black)
+            Button("Add Task") {
+                navigateToAddTask = true
+            }
+            .frame(width: 200)
+            .padding()
+            .background(Color.blue)
+            .cornerRadius(6)
+            .foregroundColor(.white)
+        }
+        .padding()
+    }
+    
+    @ViewBuilder
+    var taskListView: some View {
+        List {
+            ForEach(taskList) { task in
+                VStack(alignment: .leading) {
+                    Text(task.title)
+                        .font(.headline)
+                    Text(task.description)
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+                .swipeActions(edge: .trailing) {
+                    Button {
+                        deleteTask(task)
+                    } label: {
+                        Label("Delete", systemImage: "trash")
+                    }
+                    .tint(.red)
+                    
+                    Button {
+                        editTask(task)
+                    } label: {
+                        Label("Edit", systemImage: "pencil")
+                    }
+                    .tint(.blue)
+
+                }
+            }
+        }
+    }
+    
+    func deleteTask(_ task: TaskModel) {
+        taskList.remove(at: taskList.firstIndex(where: {$0.id == task.id})!)
+    }
+    
+    func editTask(_ task: TaskModel) {
+        mode = .edit
+        selectedTask = task
+        navigateToAddTask = true
     }
 }
 
